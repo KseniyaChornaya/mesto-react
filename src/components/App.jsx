ecf6850c51eb3dc254f3a851899e3c50c076bd77
Kseniya Chornaya <ksu.chornaya@gmail.com>
2022-09-08 18:21:28 +0300

    review

---
+++ src/components/App.jsx
@@ -0,0 +1,152 @@
+import "../index";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import PopupWithImage from "./PopupWithImage";
+import PopupWithForm from "./PopupWithForm";
+import { useState } from "react";
+
+function App() {
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
+  const [selectedCard, setSelectedCard] = useState({});
+  const [isImageOpen, setIsImageOpen] = useState(false);
+
+  function handleCardClick(obj) {
+    setIsImageOpen(true);
+    setSelectedCard(obj);
+  }
+
+  function closeAllPopups() {
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setIsEditAvatarPopupOpen(false);
+    setIsImageOpen(false);
+  }
+
+  function handleEditAvatarClick() {
+    setIsEditAvatarPopupOpen(true);
+  }
+
+  function handleEditProfileClick() {
+    setIsEditProfilePopupOpen(true);
+  }
+
+  function handleAddPlaceClick() {
+    setIsAddPlacePopupOpen(true);
+  }
+
+  return (
+    <div className="App">
+      <div className="page">
+        <Header />
+        <Main
+          onAddPlace={handleAddPlaceClick}
+          onEditProfile={handleEditProfileClick}
+          onEditAvatar={handleEditAvatarClick}
+          onCardClick={handleCardClick}
+        />
+        <Footer />
+        <PopupWithImage
+          onClose={closeAllPopups}
+          card={selectedCard}
+          isImageOpen={isImageOpen}
+        />
+        <PopupWithForm
+          title={"Редактировать профиль"}
+          name={"profile"}
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+        >
+          <input
+            required
+            placeholder="Имя"
+            id="name"
+            type="text"
+            className="popup__input popup__input_field_name"
+            name="nameInput"
+            minLength="2"
+            maxLength="40"
+          />
+          <span className="popup__input-error" id="name-error"></span>
+          <input
+            required
+            placeholder="Деятельность"
+            id="job"
+            type="text"
+            className="popup__input popup__input_field_job"
+            name="jobInput"
+            minLength="2"
+            maxLength="200"
+          />
+          <span className="popup__input-error" id="job-error"></span>
+          <button type="submit" className="popup__submit" id="editSubmit">
+            Сохранить
+          </button>
+        </PopupWithForm>
+        <PopupWithForm
+          title={"Новое место"}
+          name={"place"}
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+        >
+          <input
+            required
+            placeholder="Название"
+            id="place"
+            type="text"
+            className="popup__input popup__input_place_name"
+            name="placeNameInput"
+            minLength="2"
+            maxLength="30"
+          />
+          <span className="popup__input-error" id="place-error"></span>
+          <input
+            required
+            placeholder="Ссылка на картинку"
+            id="link"
+            type="url"
+            className="popup__input popup__input_place_link"
+            name="placeLinkInput"
+            minLength="2"
+          />
+          <span className="popup__input-error" id="link-error"></span>
+          <button type="submit" className="popup__submit" id="addSubmit">
+            Создать
+          </button>
+        </PopupWithForm>
+        <PopupWithForm title={"Вы уверены?"} name={"confirmation"}>
+          <button
+            type="submit"
+            className="popup__submit"
+            id="confirmationSubmit"
+          >
+            Да
+          </button>
+        </PopupWithForm>
+        <PopupWithForm
+          title={"Обновить аватар"}
+          name={"avatar"}
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+        >
+          <input
+            required
+            placeholder="Ссылка на новый аватар"
+            id="avatarLink"
+            type="url"
+            className="popup__input popup__input_link_avatar"
+            name="linkInput"
+            minLength="2"
+          />
+          <span className="popup__input-error" id="avatarLink-error"></span>
+          <button type="submit" className="popup__submit" id="avatarSubmit">
+            Сохранить
+          </button>
+        </PopupWithForm>
+      </div>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from '../images/logo.svg';
+
+function Header () {
+  return (
+    <header className="header">
+      <img className="header__logo" src={Logo} alt="Лого"/>
+    </header>
+  )
+}
+
+
+export default Header;
